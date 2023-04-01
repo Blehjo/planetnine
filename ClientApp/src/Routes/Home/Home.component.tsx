57748d1affe1a0907bbabae855fd758fa92d439c
bseton11-YJKnqQ <bseton11@gmail.com>
2023-04-01 18:14:29 -0500

    testing three js

--- ClientApp/src/Routes/Home/Home.component.tsx
+++ ClientApp/src/Routes/Home/Home.component.tsx
@@ -1,7 +1,10 @@
 import { Fragment } from "react"
+import { Sphere } from "../../components/Sphere/sphere.component"
 
 export const Home = () => {
     return (
-        <Fragment/>
+        <Fragment>
+            <Sphere/>
+        </Fragment>
     )
 }
